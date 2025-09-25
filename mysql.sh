36a70a43f9230b923f8e459b8245d08ac25c2fb8
keerthana-ai123 <keerthanadevathu123@gmail.com>
2025-09-25 16:39:19 +0530

    variables

--- mysql.sh
+++ mysql.sh
@@ -27,21 +27,11 @@ else
 fi
 }
 
-dnf module disable redis -y &>>$LOG_FILE
-VALIDATE $? "Disabling Default Redis"
-dnf module enable redis:7 -y &>>$LOG_FILE
-VALIDATE $? "Enabling Redis 7"
-dnf install redis -y &>>$LOG_FILE
-VALIDATE $? "Installing Redis"
+dnf install mysql-server -y
+systemctl enable mysqld
+systemctl start mysqld  
+mysql_secure_installation --set-root-pass RoboShop@1
 
-sed -i 's/127.0.0.1/0.0.0.0/g; /protected-mode/s/.*/protected-mode no/' /etc/redis/redis.conf
-
-VALIDATE $? "Allowing Remote Connections To Redis"
-
-systemctl enable redis &>>$LOG_FILE
-VALIDATE $? "Enabling Redis" 
-systemctl start redis &>>$LOG_FILE
-VALIDATE $? "Starting Redis"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
